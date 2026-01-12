d770f6bc2c6a992acc93dae1d7c973a7937d97be
javeedsir <mr.javeedsir@gmail.com>
2026-01-12 17:01:55 +0530

    Project1

---
+++ HelloApp/src/com/wipro/demo/DataTypes.java
@@ -0,0 +1,42 @@
+package com.wipro.demo;
+
+public class DataTypes {
+	
+	
+	int z;  //0
+	static  int y;  // 0
+	
+	
+
+	public static void main(String[] args) {
+
+
+		
+				int x = 0;
+		
+		
+				System.out.println(x);
+				
+				
+				double  d = 99.123456789123456789;
+				
+				System.out.println(d);
+				
+				float f = 1.123456567893456789f;
+				
+				System.out.println(f);
+				
+				char  c = ' ';
+				
+				boolean  b = false;
+				
+				
+		String s1  = null;
+		
+		System.out.println(s1);
+		
+	}
+	
+	
+
+}
---
+++ HelloApp/src/com/wipro/demo/Hello.java
@@ -0,0 +1,17 @@
+package com.wipro.demo;
+
+
+
+public class Hello{
+
+	public static void main(String[] args) {
+
+
+			System.out.println("Hello World , Welcome");
+			
+		
+		    System.out.println(Math.random());
+		
+	}
+
+}
