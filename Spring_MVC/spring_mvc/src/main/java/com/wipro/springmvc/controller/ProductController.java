1893dd3a3e8d0787810adce209493479e1b02cbe
javeed512 <javeedmohammedjaveed@gmail.com>
2026-02-09 13:43:23 +0530

    spring mvc demo , notes

---
+++ Spring_MVC/spring_mvc/src/main/java/com/wipro/springmvc/controller/ProductController.java
@@ -0,0 +1,46 @@
+package com.wipro.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.wipro.springmvc.entity.Product;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/estore")
+public class ProductController {
+
+	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
+	@ResponseBody
+	public String welcome() {
+
+		return "<h1  style='color:red'>WELCOME CUSTOMERS</h1>";
+	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public String addProduct(HttpServletRequest request, HttpServletResponse response, HttpSession session) {
+
+		int pid = Integer.parseInt(request.getParameter("pid"));
+		String pname = request.getParameter("pname");
+		double price = Double.parseDouble(request.getParameter("price"));
+
+		Product product = new Product();
+
+		product.setPid(pid);
+		product.setPname(pname);
+		product.setPrice(price);
+		
+		
+		session.setAttribute("product", product);
+		
+
+		return "display"; // display.jsp
+
+	}
+
+}
