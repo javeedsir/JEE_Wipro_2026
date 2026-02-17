5c34c50cdc1c2866794b20560f2b9b001da10d38
javeed512 <javeedmohammedjaveed@gmail.com>
2026-02-17 17:40:16 +0530

    lombok demo

---
+++ lombok.demo/src/main/java/com/wipro/lombok/demo/App.java
@@ -0,0 +1,26 @@
+package com.wipro.lombok.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        Employee e1 = new Employee();
+        
+        e1.setEid(102);
+        
+        System.out.println(e1.getEid());
+        
+        Employee e2 = new Employee(1, "javeed", 7000);
+        
+        
+        System.out.println(e2.toString());
+        
+        
+    }
+}
